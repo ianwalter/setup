c085f74be4bd1a04879d3a545ee3e978f7717b1b
Ian Walter <122028+ianwalter@users.noreply.github.com>
2025-03-07 00:10:10 -0500

    Adding zsh config

--- setup.ts
+++ setup.ts
@@ -7,6 +7,7 @@ await $`curl -fsSL https://bun.sh/install | bash`;
 await $`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`;
 
 // Install Homebrew Formulae.
+await $`brew install eza` // A better ls command.
 await $`brew install gh`; // GitHub CLI.
 await $`brew install git`; // Source control.
 await $`brew install go`; // Go language.
@@ -35,7 +36,61 @@ await $`brew install zed`;
 await $`brew install zen-browser`;
 await $`brew install zoom`;
 
-//
+// Copy zsh configuration to ~/.zshrc.
+const zshrc =
+`# Setup word skipping using <Alt><RightArrow> and <Alt><LeftArrow> to behave
+# like macOS.
+# https://stackoverflow.com/questions/12382499/looking-for-altleftarrowkey-solution-in-zsh
+bindkey '[C' forward-word
+bindkey '[D' backward-word
 
-//
-// await $`source ~/.zshrc`
+# Setup command history cycling using <Alt><UpArrow> and <Alt><DownArrow>.
+bindkey '[A' history-beginning-search-backward
+bindkey '[B' history-beginning-search-forward
+
+# Setup aliases to use alternative utilities.
+alias ls='eza'
+alias rm='trash'
+alias cd='z'
+
+# Configure zsh command history.
+# File to save command history.
+HISTFILE=~/.zsh_history
+# How many commands are loaded into shell memory.
+HISTSIZE=5000
+# How many commands to save to the history file.
+SAVEHIST=5000
+# Append history to the history file (don't overwrite).
+setopt APPEND_HISTORY
+# Immediately append to the history file, not just when a terminal is killed.
+setopt INC_APPEND_HISTORY
+# Share history across sessions.
+setopt SHARE_HISTORY
+# Also save when the command started and how long it ran for.
+setopt EXTENDED_HISTORY
+# Don't save a history line if it's the same as the previous one.
+setopt HIST_IGNORE_DUPS
+# When history fills up, remove duplicate commands first.
+setopt HIST_EXPIRE_DUPS_FIRST
+# Remove meaningless whitespace from command history.
+setopt HIST_REDUCE_BLANKS
+
+# Set vim as the default editor.
+export EDITOR=vim
+
+# Add binaries to the PATH so they are always accessible.
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+export PATH="/opt/homebrew/opt/trash/bin:$PATH"
+export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"
+
+# Setup zoxide completions.
+eval "$(zoxide init zsh)"
+
+# Use the Starship prompt.
+eval "$(starship init zsh)"
+`;
+await $`echo ${zshrc} > ~/.zshrc`;
+
+// Activate zsh configuration.
+await $`source ~/.zshrc`
